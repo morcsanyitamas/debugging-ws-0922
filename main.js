2e46bbc90da7510ff2b19bbf4021cd214d19dc83
morcsanyitamas <mtomi8@gmail.com>
2022-09-22 12:01:05 +0200

    completed example1

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 // n! = n * (n - 1) * (n - 2) * ... (1)
-// 5! = 5 * 4 * 3 * 2 * 1 
+// 5! = 5 * 4 * 3 * 2 * 1
 
-function calcFact(nuber){
-    ;
+
+// function calcFact(number) {
+//     for (let i = number - 1; i >= 1; i--) {
+//         number *= i;
+//     }
+//     return number;
+// }
+
+// console.log(calcFact(5));
+
+// function calcFact(number){
+//     for (let i = number - 1; i >= 1; i--){
+//         number*=i;
+//     }
+//     return number;
+// }
+// console.log(calcFact(5));
+
+// n! = n * (n - 1)!
+const calcFact = (number) => {
+    if (number === 1){
+        return 1
+    } else {
+        return number * calcFact(number - 1);
+    }
 }
+
+console.log(calcFact(5));
